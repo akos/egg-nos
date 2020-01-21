3c9152014b80ec4744b352c00490160a5c3cfe57
林冠夫 <lgfshr@163.com>
2020-01-21 15:17:00 +0800

    :memo: update the loader

--- test/fixtures/apps/nos-test/config/config.default.js
+++ test/fixtures/apps/nos-test/config/config.default.js
@@ -2,8 +2,8 @@
 
 exports.keys = '123456';
 exports.nos = {
-  accessId: '', // accessKey
-  secretId: '', // secreId
+  accessId: 'ab84d60b4d3141eb9f22161cc20f5f0e',
+  secretId: 'bb8185a881134cb7b5303141ceef02e1',
   endpoint: 'nos-eastchina1.126.net',
   bucket: 'testnos',
   port: '80',
